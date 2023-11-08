20e707ee16e4e7b8903ae5f8d313414f794525e3
sujan-22 <sujanrokad44@gmail.com>
2023-11-07 21:24:18 -0500

    Add project files.

---
+++ Lab4/Employee.cs
@@ -0,0 +1,51 @@
+﻿//I, Sujan Rokad, 000882948 certify that this material is my origianl work. No other person's work has been used without due acknowledgement.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Employee
+    {
+        // Private fields to store employee information
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public decimal Rate { get; set; }
+        public double Hours { get; set; }
+
+        // Constructor to initialize employee information
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+            Name = name;
+            Number = number;
+            Rate = rate;
+            Hours = hours;
+        }
+
+        // Getter methods
+        public decimal GetGross()
+        {
+            // Calculate gross pay with overtime
+            if (Hours > 40)
+            {
+                double regularHours = 40;
+                double overtimeHours = Hours - regularHours;
+                return (decimal)((regularHours * (double)Rate) + (overtimeHours * (double)Rate * 1.5));
+            }
+            else
+            {
+                return (decimal)(Hours * (double)Rate);
+            }
+        }
+        
+        // ToString method to format employee information
+        public override string ToString()
+        {
+            return $"{Name,-21} {Number,-(int)9.5} {Rate,-(int)9.5:C} {Hours,-8:F2} {GetGross(),-(int)9.5:C}";
+        }
+    }
+
+}
+
---
+++ Lab4/Program.cs
@@ -0,0 +1,177 @@
+﻿///I, Sujan Rokad, 000882948 certify that this material is my origianl work. No other person's work has been used without due acknowledgement.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+
+    /// <summary>
+    /// This program reads employee data from a CSV file, sorts it based on different criteria,
+    /// and displays the sorted data in a table.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Employee list to store employee data.
+        /// </summary>
+        private List<Employee> employees = new List<Employee>();
+
+        /// <summary>
+        /// A method that is responsible for reading data from CSV file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Read(string filePath)
+        {
+            try
+            {
+                using (StreamReader data = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = data.ReadLine()) != null)
+                    {
+                        // An array of string object that split each CSV file line with comma and stores it in employeeData array.
+                        string[] employeeData = line.Split(',');
+                        
+                        // If statement that checks if splitted data that stored in array has length of 4 and if it does then goes into if condition.
+                        if (employeeData.Length == 4)
+                        {
+                            string name = employeeData[0].Trim(); // Extract the employee name from the first element of 'employeeData' array and stores it in 'name' variable
+                            int number = int.Parse(employeeData[1].Trim()); // Parse the employee number from the second element of 'employeeData' array and store it as an integer
+                            decimal rate = decimal.Parse(employeeData[2].Trim()); // Parse the employee pay rate from the third element of 'employeeData' array and store it as a decimal
+                            double hours = double.Parse(employeeData[3].Trim()); // Parse the employee hours worked from the fourth element of 'employeeData' array and store it as a double
+
+                            employees.Add(new Employee(name, number, rate, hours)); // Create and add new Employee object with the parsed name, number, rate, and hours, then add it in the 'employees' list
+                        }
+                    }
+                }
+            } 
+            // catch block to handle an exception occurs during file reading or parsing
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to read file:");
+                Console.WriteLine(e.Message); //display the specific error message
+            }
+        }
+
+        /// <summary>
+        /// Display the sorted employee data in a table
+        /// </summary>
+        public void DisplayTable()
+        {
+            // Display a horizontal line of '=' characters to separate the table visually
+            Console.WriteLine(new string('=', 65));
+            
+            // Display the table header with column headers and formatting
+            Console.WriteLine($"| {"Name",-21} {"Number",-(int)9.5} {"Rate",-(int)9.5} {"Hours",-8} {"Gross Pay  | ",-(int)9.5}");
+
+            // Display another horizontal line of '=' characters for clarity
+            Console.WriteLine(new string('=', 65));
+
+            // Loop through each employee in the sorted list
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"| {employee}  |");
+            }
+
+            // Display a final horizontal line of '=' characters to complete the table
+            Console.WriteLine(new string('=', 65));
+        }
+
+        public static void Main(string[] args)
+        {
+            // Create an instance of the Program class
+            Program lab = new Program();
+
+            // Read employee data from the CSV file
+            lab.Read("employees.txt");
+
+            // Initialize a boolean variable to control program exit
+            bool exit = false;
+
+            // Display a menu to the user and handle their choice
+            while (!exit)
+            {
+                // Define a formatted text menu with options
+                string menu =
+$@"
+------------------------------------------|
+Choose any from below options             |
+------------------------------------------|
+1) Sort by Employee Name (ascending)      |
+2) Sort by Employee Number (ascending)    |  
+3) Sort by Employee Pay Rate (descending) |
+4) Sort by Employee Hours (descending)    |
+5) Sort by Employee Gross Pay (descending)|
+6) Exit                                   |
+------------------------------------------|
+Enter your choice: ";     
+
+                // Display the menu to the user
+                Console.Write(menu);
+
+                // variable to store the user's choice
+                int choice;
+
+                // Attempt to read the user's choice from the console and convert into an integer
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    // Handle the user's choice based on the selected option
+                    switch (choice)
+                    {
+                        // Sort employees by name in ascending order
+                        case 1:
+                            lab.employees.Sort((e1, e2) => e1.Name.CompareTo(e2.Name));
+                            lab.DisplayTable();
+                            break;
+
+                        // Sort employees by number in ascending order
+                        case 2:
+                            lab.employees.Sort((e1, e2) => e1.Number.CompareTo(e2.Number));
+                            lab.DisplayTable();
+                            break;
+                        
+                        // Sort employees by pay rate in descending order
+                        case 3:
+                            lab.employees.Sort((e1, e2) => e2.Rate.CompareTo(e1.Rate));
+                            lab.DisplayTable();
+                            break;
+                        
+                        // Sort employees by hours in descending order
+                        case 4:
+                            lab.employees.Sort((e1, e2) => e2.Hours.CompareTo(e1.Hours));
+                            lab.DisplayTable();
+                            break;
+                        
+                        // Sort employees by gross pay in descending order
+                        case 5:
+                            lab.employees.Sort((e1, e2) => e2.GetGross().CompareTo(e1.GetGross()));
+                            lab.DisplayTable();
+                            break;
+
+                        // Set the 'exit' flag to true, exiting the program    
+                        case 6:
+                            exit = true;
+                            Console.WriteLine("Ciao. "); // Goodbye message
+                            Thread.Sleep(2000); // Waits for 2 seconds before exiting
+                            break;
+
+                        // Default case to display an error message for an invalid choice
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid choice.");
+                }
+            }
+        }
+    }
+
+}
